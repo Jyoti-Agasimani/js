79378fc33957359a034153c021156167cc44e00f
Jyoti-Agasimani <134587300+Jyoti-Agasimani@users.noreply.github.com>
2024-11-15 08:45:44 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -50,11 +50,37 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "Jyoti"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "jyoti",
+//     "coursename": "js",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
 
